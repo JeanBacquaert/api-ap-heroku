709ca511532ae71a628804a18cb90312d0987392
JeanBacquaert <79642258+JeanBacquaert@users.noreply.github.com>
2022-03-04 15:33:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://tender-pike-d19ce0.netlify.app/'
+        origin: 'https://tender-pike-d19ce0.netlify.app'
     })
 );
 app.options('*', cors());
