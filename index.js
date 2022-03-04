a2dfafb184f4e20a7ad924644ed6ad0e99659728
JeanBacquaert <79642258+JeanBacquaert@users.noreply.github.com>
2022-03-04 14:54:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://cocky-snyder-5a02cf.netlify.app'
+        origin: 'https://zealous-wing-5fcfbe.netlify.app/'
     })
 );
 app.options('*', cors());
